f6455cbac1ff1ad09157b9c32039b08670f49a0e
akshat4619 <akshatjn4619@gmail.com>
2025-07-22 19:11:16 +0530

    Added a global exception handler to handle all custom exceptions

---
+++ aipresentationbackend/aipresentationbackend/src/main/java/com/hackathon/aipresentationbackend/model/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.hackathon.aipresentationbackend.model;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String errorCode;
+    private String message;
+    private Map<String,String> validationErrors;
+}
